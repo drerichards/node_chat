1d1c15df37f1dad79f5f91fe2ecc8c0deb92ffe8
Andre Richardson <andrerichards@Andres-MacBook-Pro.local>
2018-06-09 21:16:34 -0400

    add mongo mongoose connect

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const mongoose = require('mongoose')
+const dbUrl = 'mongodb://user:user22@ds153980.mlab.com:53980/node-chat'
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
@@ -18,11 +20,18 @@ app.get('/messages', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
-    console.log(messages)
     messages.push(req.body)
+    io.emit('message', req.body)
     res.sendStatus(200)
 })
 
+io.on('connection', (socket) => {
+    console.log('socket connected')
+})
+
+mongoose.connect(dbUrl, err => {
+    err ? console.log(err) : console.log('mongo connected')
+})
 const server = http.listen(3000, () => {
     console.log('server on port', server.address().port)
 })
