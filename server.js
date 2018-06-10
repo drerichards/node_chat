0440733d252b72cafc639fce69a540f76292e0ee
Andre Richardson <andrerichards@Andres-MacBook-Pro.local>
2018-06-09 23:46:08 -0400

    set up promise for db censor

--- server.js
+++ server.js
@@ -8,22 +8,48 @@ const dbUrl = 'mongodb://user:user22@ds153980.mlab.com:53980/node-chat'
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
-const messages = [
-    {name: 'Tim', message: 'hi'},
-    {name: 'Marty', message: 'hey'}
-]
+mongoose.Promise = Promise
+
+const Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        err ? console.log(err) : res.send(messages)
+    })
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    const message = new Message(req.body)
+    message.save()
+        .then(() => {
+            console.log('saved')
+            return Message.findOne({
+                message: 'bad word'
+            })
+        }).then(censored => {
+            if (censored) {
+                console.log('Censored Word: ', censored)
+                return Message.remove({
+                    _id: censored.id
+                }, err => {
+                    console.log('Removed censored message')
+                })
+            }
+            io.emit('message', req.body)
+            res.sendStatus(200)
+        }).catch(err => {
+            res.sendStatus(500)
+            return console.error(err)
+        })
 })
+
 
 io.on('connection', (socket) => {
     console.log('socket connected')
