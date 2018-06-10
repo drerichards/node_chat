af3a319ed2518b361fdde4455d5bb557678abeae
Andre Richardson <andrerichards@Andres-MacBook-Pro.local>
2018-06-09 20:16:36 -0400

    post messages/setup ocket.io

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 const messages = [
     {name: 'Tim', message: 'hi'},
@@ -12,6 +17,12 @@ app.get('/messages', (req, res) => {
     res.send(messages)
 })
 
-const server = app.listen(3000, () => {
+app.post('/messages', (req, res) => {
+    console.log(messages)
+    messages.push(req.body)
+    res.sendStatus(200)
+})
+
+const server = http.listen(3000, () => {
     console.log('server on port', server.address().port)
 })
